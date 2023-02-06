2bc339c0c4a890c6b595f6377c58a1e66f192e9f
test <astappleid@itstep.kz>
2023-02-06 15:38:39 +0600

    first commit

---
+++ Api/ApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiApp.swift
+//  Api
+//
+//  Created by swift on 06.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct ApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
